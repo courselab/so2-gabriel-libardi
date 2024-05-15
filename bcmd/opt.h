80c49d1f35c0de3024dcfe6d16e77f869c8c0e77
gabriel-libardi <libardi.120205@protonmail.com>
2024-05-15 00:06:07 -0300

    Complleted both exercises

---
+++ bcmd/opt.h
@@ -0,0 +1,17 @@
+/*
+ *    SPDX-FileCopyrightText: 2024 Matheus Pereira Dias <matheuspd07@gmail.com>
+ *    SPDX-FileCopyrightText: 2024 Gabriel Franceschi Libardi <libardi.120205@protonmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *    This file is part of SYSeg, available at https://gitlab.com/monaco/syseg.
+ */
+
+#ifndef OPT_H
+#define OPT_H
+
+int __attribute__((fastcall)) strcmp(const char *, const char *);
+int __attribute__((fastcall)) strlen(const char *);
+int __attribute__((fastcall)) itoa(unsigned int, char *);
+
+#endif  /* OPT_H  */
